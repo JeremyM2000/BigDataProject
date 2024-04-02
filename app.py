a1fffeb3a93ec489c2367908856edb5a0691edad
JeremyM2000 <jeremy.meme@hotmail.fr>
2024-04-02 11:51:44 +0200

    add dockerfile, requirments.txt, license

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ import os
 import uuid
 from flask import Flask, flash, request, redirect, send_from_directory
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
+    @app.route('/')
+    def root():
+        return app.send_static_file('index.html')
 
-@app.route('/')
-def root():
-    return app.send_static_file('index.html')
+    return app
 
+flask_app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    flask_app.run(port=80, host="0.0.0.0", debug=True)
