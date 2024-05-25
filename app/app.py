3c68888824682f54e74df8e620f19ef3c630e19d
pelleservan <pelle.servan@hotmail.fr>
2024-05-25 12:10:10 +0200

    update app logic to play with several players

--- app/app.py
+++ app/app.py
@@ -48,8 +48,8 @@ def login():
 
         if user and user.password == password:
             session['user_name'] = user.name
-            session['players'] = [user.name]
             session['player_id'] = user.id
+            session['players'] = [user.name]
             flash('Logged in successfully')
             return redirect(url_for('home'))
         else:
@@ -60,8 +60,10 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('user_name', None)
-    session.pop('players', None) 
+    session.pop('player_id', None) 
+    session.pop('players', None)
     session.pop('questions_per_player', None)
+    session.pop('current_player_index', None)
     flash('You have been logged out')
     return redirect(url_for('index'))
 
@@ -70,13 +72,14 @@ def home():
     session.pop('random_question_ids', None)
     session.pop('current_question_index', None)
     session.pop('quiz_id', None)
+    session.pop('quiz_results', None)
+    session.pop('players', None)
     
     if 'user_name' in session:
         user_name = session['user_name']
         return render_template('home.html', user_name=user_name)
     else:
         return redirect(url_for('login'))
-
 
 @app.route('/set_quiz', methods=['GET', 'POST'])
 def set_quiz():
@@ -94,11 +97,23 @@ def add_player():
     
     return redirect(url_for('set_quiz'))
 
+@app.route('/remove_player/<player_name>')
+def remove_player(player_name):
+    if 'players' in session:
+        players = session['players']
+        if player_name in players and players.index(player_name) != 0:
+            players.remove(player_name)
+            flash(f'Player {player_name} removed')
+        else:
+            flash('Cannot remove the first player')
+    return redirect(url_for('set_quiz'))
+
 @app.route('/start_quiz', methods=['GET', 'POST'])
 def start_quiz():
     questions_per_player = request.form['questions_per_player']
     session['questions_per_player'] = questions_per_player
-    return render_template('quiz_starter.html', players=session.get('palyers', []), questions_per_player=questions_per_player)
+    session['current_player_index'] = 0
+    return redirect(url_for('quiz'))
 
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
@@ -117,7 +132,6 @@ def quiz():
         session['random_question_ids'] = random_question_ids
         session['current_question_index'] = 0
         
-        # Determine the quiz_id for the current quiz
         max_quiz_id = db.session.query(func.max(AskedQuestion.quiz_id)).scalar() or 0
         session['quiz_id'] = max_quiz_id + 1
     
@@ -133,12 +147,28 @@ def quiz():
 
         bool_aws = selected_answer == question.correct_aws
 
-        player_id = session.get('player_id')
-        
-        asked_question = AskedQuestion(quiz_id=quiz_id, question_id=question_id, player_id=player_id, bool_aws=bool_aws)
+        current_player_index = session['current_player_index']
+        players = session['players']
+        current_player = players[current_player_index]
+
+        asked_question = AskedQuestion(quiz_id=quiz_id, question_id=question_id, player_id=session.get('player_id', 0), bool_aws=bool_aws)
         db.session.add(asked_question)
         
+        if 'quiz_results' not in session:
+            session['quiz_results'] = {}
+
+        if current_player not in session['quiz_results']:
+            session['quiz_results'][current_player] = []
+
+        session['quiz_results'][current_player].append({
+            'question': question.question,
+            'selected_answer': selected_answer,
+            'correct_answer': question.correct_aws,
+            'is_correct': bool_aws
+        })
+
         session['current_question_index'] = current_index + 1
+        session['current_player_index'] = (current_player_index + 1) % len(players)
         
         db.session.commit() 
         
@@ -150,28 +180,27 @@ def quiz():
     question_id = random_question_ids[current_index]
     question = Question.query.get(question_id)
 
-    return render_template('question.html', question=question)
+    current_player_index = session['current_player_index']
+    players = session['players']
+    current_player = players[current_player_index]
+
+    return render_template('question.html', question=question, player=current_player)
 
 @app.route('/quiz_summary')
 def quiz_summary():
-    quiz_id = session.get('quiz_id')
-    player_id = session.get('player_id')
+    quiz_results = session.get('quiz_results', {})
     
-    asked_questions = AskedQuestion.query.filter_by(quiz_id=quiz_id, player_id=player_id).all()
-    
-    score = sum(1 for question in asked_questions if question.bool_aws)
-    
-    question_details = []
-    
-    for question in asked_questions:
-        question_info = {
-            'question': Question.query.get(question.question_id).question,
-            'selected_answer': question.bool_aws,
-            'correct_answer': Question.query.get(question.question_id).correct_aws
-        }
-        question_details.append(question_info)
+    player_scores = []
+    for player_name, results in quiz_results.items():
+        score = sum(1 for result in results if result['is_correct'])
+        
+        player_scores.append({
+            'player': player_name,
+            'score': score,
+            'question_details': results
+        })
 
-    return render_template('quiz_summary.html', score=score, question_details=question_details)
+    return render_template('quiz_summary.html', player_scores=player_scores)
 
 from flask import render_template
 from app.models.user import User
